a0f0ce152fe2d0c303e733d1a8f7a9eb9002c799
Ermesson <ermessonlimadossantos@hotmail.com>
2023-04-28 11:50:49 -0300

    refactor: Rename file

--- src/Screens/HomeScreem/HomeScreen.tsx
+++ src/Screens/HomeScreem/HomeScreen.tsx
@@ -2,7 +2,6 @@ import React, { ReactElement } from 'react'
 
 import { useHomeController } from './HomeScreen.controller'
 import Background from '../../components/Background/Background'
-import Weather from '../../components/Box/Weather'
 import Card from '../../components/Card/Card'
 import DailyTemperature from '../../components/DailyTemperature/DailyTemperature'
 import Environmental from '../../components/Environmental/Environmental'
@@ -10,6 +9,7 @@ import Header from '../../components/Header/Header'
 import { LoadingRequest } from '../../components/Loading/LoadingRequest'
 import { ModalizeLocation } from '../../components/Modalize/Modalize'
 import NextForecast from '../../components/NextForecast/NextForecast'
+import Weather from '../../components/Weather/Weather'
 
 export default function HomeScreen(): ReactElement {
   const { modalizeRef, onOpen, isLoading } = useHomeController()
